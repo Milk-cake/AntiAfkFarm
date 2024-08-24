983b6e192acaf2fecc6e6713a88ac2b36790f339
Luna <>
2024-08-24 14:47:06 +1000

    refactor: Removed unused imports

--- src/main/java/icu/redge/antiafkfarm/EventListener.java
+++ src/main/java/icu/redge/antiafkfarm/EventListener.java
@@ -1,18 +1,10 @@
 package icu.redge.antiafkfarm;
 
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.damage.DamageSource;
-import org.bukkit.entity.Damageable;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.LivingEntity;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.plugin.Plugin;
-import org.bukkit.loot.Lootable;
 
 import java.util.*;
 
